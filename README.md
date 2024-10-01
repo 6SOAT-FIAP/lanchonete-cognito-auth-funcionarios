5d0fe1cca76d80cec3a6cc36fd2f9164ca653278
DeividCezar <perspicilum@gmail.com>
2024-10-01 13:38:17 -0300

    user pool para funcionarios

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# lanchonete-cognito-auth-funcionarios
+# Cognito auth
+
+User pool para autenticação dos funcionários com base no CPF.
+
+## Objetivo
+
+Há uma lanchonete de bairro que está se expandindo devido ao seu grande sucesso.
+
+Nesse contexto, um sistema de controle de pedidos é essencial para garantir que a lanchonete possa atender os clientes
+de maneira eficiente, gerenciando seus pedidos e estoques de forma adequada.
+
+Para solucionar o problema, o projeto visa oferecer à lanchonete um sistema de autoatendimento de fast food que permite
+aos clientes selecionar e fazer pedidos sem precisar interagir com um atendente.
+
+O Cognito User Pool criado por este repositório compõe o fluxo de autenticação dos funcionários.
+
+## Collection
+
+Acesse a [**collection**](assets/autenticacao.postman_collection.json) do Postman com todas as chamadas ao Cognito.
+
+## Funcionário balcão
+
+Trata-se de um funcionário genérico auto-criado, o username (CPF) neste cenário é 00000000000. 
+
+Cabe destacar também que a criação de usuários ficou bloqueada aos administradores.
---
+++ infra/main.tf
@@ -0,0 +1,44 @@
+resource "aws_cognito_user_pool" "funcionarios_lanchonete_user_pool" {
+  name              = var.user_pool_name
+  mfa_configuration = var.mfa_configuration
+
+  schema {
+    name                = "name"
+    attribute_data_type = "String"
+    required            = true
+  }
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+  }
+
+  password_policy {
+    minimum_length    = 8
+    require_uppercase = true
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+  }
+
+    admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+}
+
+resource "aws_cognito_user_pool_client" "lanchonete_user_pool_client" {
+  name            = "lanchonete-client-funcionarios"
+  user_pool_id    = aws_cognito_user_pool.funcionarios_lanchonete_user_pool.id
+  generate_secret = false
+}
+
+resource "aws_cognito_user" "funcionario_balcao" {
+  user_pool_id = aws_cognito_user_pool.funcionarios_lanchonete_user_pool.id
+  username     = "00000000000"
+
+  attributes = {
+    name  = "Funcionario balcao"
+    email = "balcao@gmail.com"
+  }
+}
